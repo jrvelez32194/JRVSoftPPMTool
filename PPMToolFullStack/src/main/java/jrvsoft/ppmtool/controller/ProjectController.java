45838a4df70ea8c947a01932e0ad07165e9f0e99
john <johnrexcyvelez@gmail.com>
2022-04-14 02:02:44 +0800

    Branch 4

--- PPMToolFullStack/src/main/java/jrvsoft/ppmtool/controller/ProjectController.java
+++ PPMToolFullStack/src/main/java/jrvsoft/ppmtool/controller/ProjectController.java
@@ -5,6 +5,7 @@ import jrvsoft.ppmtool.services.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +21,13 @@ public class ProjectController {
     private final ProjectService projectService;
 
     @PostMapping()
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
-        return new ResponseEntity<Project>(projectService.saveOrUpdate(project), HttpStatus.CREATED);
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+
+        if(result.hasErrors()){
+            return new ResponseEntity<String>("Invalid object", HttpStatus.BAD_REQUEST);
+        } else {
+            return new ResponseEntity<Project>(projectService.saveOrUpdate(project), HttpStatus.CREATED);
+        }
     }
 
 }
