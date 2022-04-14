80800807435a2d9078b24305f67b6bcebc871ad5
john <johnrexcyvelez@gmail.com>
2022-04-15 00:56:56 +0800

    branch14

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Dashboard from "./component/Dashboard";
 import Header from "./component/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
