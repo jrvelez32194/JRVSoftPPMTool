f787c5ce7cadc3eb65d56c980e4149744e2bb216
john <johnrexcyvelez@gmail.com>
2022-04-17 11:01:09 +0800

    branch 29

--- ppmtool-react-client/src/reducers/index.js
+++ ppmtool-react-client/src/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from "redux";
 import errorReducer from "./errorReducer";
 import projectReducer from "./projectReducer";
-import reducer from "./reducer";
 
 export default combineReducers({
   errors: errorReducer,
