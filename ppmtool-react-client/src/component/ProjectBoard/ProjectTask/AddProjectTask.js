533538bc34de3cfdaf3a2a7b31acbec7502e64d4
john <johnrexcyvelez@gmail.com>
2022-04-18 15:57:47 +0800

    branch47

--- ppmtool-react-client/src/component/ProjectBoard/ProjectTask/AddProjectTask.js
+++ ppmtool-react-client/src/component/ProjectBoard/ProjectTask/AddProjectTask.js
@@ -18,9 +18,39 @@ class AddProjectTask extends Component {
       projectIdentifier: id,
       errors: {},
     };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  //life cycle hooks
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const newProjectTask = {
+      summary: this.state.summary,
+      acceptanceCriteria: this.state.acceptanceCriteria,
+      status: this.state.status,
+      priority: this.state.priority,
+      dueDate: this.state.dueDate,
+    };
+    this.props.createProjectTask(
+      this.state.projectIdentifier,
+      newProjectTask,
+      this.props.history
+    );
   }
   render() {
     const { id } = this.props.match.params;
+    const { errors } = this.state;
     return (
       <div className="add-PBI">
         <div className="container">
@@ -35,53 +65,88 @@ class AddProjectTask extends Component {
                 <div className="form-group">
                   <input
                     type="text"
-                    className="form-control form-control-lg"
+                    className={classNames("form-control form-control-lg", {
+                      "is-invalid": errors.summary,
+                    })}
                     name="summary"
                     placeholder="Project Task summary"
                     value={this.state.summary}
+                    onChange={this.onChange}
                   />
+                  {errors.summary && (
+                    <div className="invalid-feedback">{errors.summary}</div>
+                  )}
                 </div>
+                <br />
                 <div className="form-group">
                   <textarea
-                    className="form-control form-control-lg"
+                    className={classNames("form-control form-control-lg", {
+                      "is-invalid": errors.acceptanceCriteria,
+                    })}
                     placeholder="Acceptance Criteria"
                     name="acceptanceCriteria"
                     value={this.state.acceptanceCriteria}
+                    onChange={this.onChange}
                   ></textarea>
+                  {errors.acceptanceCriteria && (
+                    <div className="invalid-feedback">
+                      {errors.acceptanceCriteria}
+                    </div>
+                  )}
                 </div>
+                <br />
                 <h6>Due Date</h6>
                 <div className="form-group">
                   <input
                     type="date"
-                    className="form-control form-control-lg"
+                    className={classNames("form-control form-control-lg", {
+                      "is-invalid": errors.dueDate,
+                    })}
                     name="dueDate"
                     value={this.state.dueDate}
+                    onChange={this.onChange}
                   />
+                  {errors.dueDate && (
+                    <div className="invalid-feedback">{errors.dueDate}</div>
+                  )}
                 </div>
+                <br />
                 <div className="form-group">
                   <select
-                    className="form-control form-control-lg"
+                    className={classNames("form-control form-control-lg", {
+                      "is-invalid": errors.priority,
+                    })}
                     name="priority"
                     value={this.state.priority}
+                    onChange={this.onChange}
                   >
                     <option value={0}>Select Priority</option>
                     <option value={1}>High</option>
                     <option value={2}>Medium</option>
                     <option value={3}>Low</option>
                   </select>
+                  {errors.priority && (
+                    <div className="invalid-feedback">{errors.priority}</div>
+                  )}
                 </div>
-
+                <br />
                 <div className="form-group">
                   <select
-                    className="form-control form-control-lg"
+                    className={classNames("form-control form-control-lg", {
+                      "is-invalid": errors.status,
+                    })}
                     name="status"
                     value={this.state.status}
+                    onChange={this.onChange}
                   >
                     <option value="">Select Status</option>
                     <option value="TO_DO">TO DO</option>
                     <option value="IN_PROGRESS">IN PROGRESS</option>
                     <option value="DONE">DONE</option>
                   </select>
+                  {errors.status && (
+                    <div className="invalid-feedback">{errors.status}</div>
+                  )}
                 </div>
 
                 <input
@@ -101,4 +166,8 @@ AddProjectTask.propTypes = {
   createProjectTask: PropTypes.func.isRequired,
 };
 
-export default connect(null, { createProjectTask })(AddProjectTask);
+const mapStateToProps = (state) => ({
+  errors: state.errors,
+});
+
+export default connect(mapStateToProps, { createProjectTask })(AddProjectTask);
