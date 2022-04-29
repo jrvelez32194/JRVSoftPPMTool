8df6d28d091e4e5853e63197c3f315160603abd5
john <johnrexcyvelez@gmail.com>
2022-04-29 11:28:03 +0800

    adding security class vs code

--- ppmtool-react-client/src/actions/securityActions.js
+++ ppmtool-react-client/src/actions/securityActions.js
@@ -1,5 +1,8 @@
 import axios from "axios";
+import setJWTToken from "../securityUtils/setJWTToken";
 import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import jwtDecode from "jwt-decode";
+import { type } from "@testing-library/user-event/dist/type";
 
 export const createUser = (newUser, history) => async (dispatch) => {
   try {
@@ -8,6 +11,25 @@ export const createUser = (newUser, history) => async (dispatch) => {
     dispatch({
       type: GET_ERRORS,
       payload: {},
+    });
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data,
+    });
+  }
+};
+
+export const login = (LogRequest) => async (dispatch) => {
+  try {
+    const res = await axios.post("/api/user/login", LogRequest);
+    const { token } = res.data;
+    localStorage.setItem("jwtToken", token);
+    setJWTToken(token);
+    const decoded = jwtDecode(token);
+    dispatch({
+      type: SET_CURRENT_USER,
+      payload: decoded,
     });
   } catch (err) {
     dispatch({
